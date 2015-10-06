3037c053c5a35e55483ebfc53357362508b92b4f
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2015-10-05 17:57:55 -0700

    added controller actions to post controller and a category controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,46 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first  # change once authentication implemented
 
+    if @post.save
+      flash[:notice] = "Your post was successfully created!"
+      redirect_to post_path(@post)  # or just @post
+    else
+      render :new
+    end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "Your post was successfully updated!"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
